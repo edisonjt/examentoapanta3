3ea7e53a19677a07843ff4403e2560b44aec5c2f
edisonjt <eddyt1999@gmail.com>
2024-03-11 19:26:52 -0500

    first commit

---
+++ src/main/java/ec/edu/espe/examentoapanta/examentoapanta/ExamentoapantaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examentoapanta.examentoapanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamentoapantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamentoapantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/examentoapanta/examentoapanta/ExamentoapantaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examentoapanta.examentoapanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamentoapantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
